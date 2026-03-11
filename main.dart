6b34b559542550697999fce886a5dcce11ce8072
rojalingga <rojalingga2006@gmail.com>
2026-03-11 09:48:41 +0700

    feat : add loop to display number sequence

--- main.dart
+++ main.dart
@@ -4,9 +4,22 @@ void main() {
   stdout.write("Masukkan sebuah angka: ");
   int angka = int.parse(stdin.readLineSync()!);
 
+  cekGanjilGenap(angka);
+  tampilkanDeret(angka);
+}
+
+void cekGanjilGenap(int angka) {
   if (angka % 2 == 0) {
     print("$angka adalah bilangan GENAP");
   } else {
     print("$angka adalah bilangan GANJIL");
+  }
+}
+
+void tampilkanDeret(int angka) {
+  print("Deret angka dari 1 sampai $angka:");
+
+  for (int i = 1; i <= angka; i++) {
+    print(i);
   }
 }
